a826d9e11c277b4e4de5453e96d0bc28ed84370c
Faris Kouetessa <90173271+farisbrandone@users.noreply.github.com>
2024-10-06 08:03:32 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://www.mamaisonalouer.com/sitemap.xml',
+    sitemap: 'https://ma-maison-a-louer-com.vercel.app/sitemap.xml',
   }
 }
